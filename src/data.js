150cd7d2494718fa2ff35354151ca3b87e0c309c
J1NX-82 <rahulgoala082.gmail.com>
2024-07-09 10:28:02 +0530

    Final

---
+++ src/data.js
@@ -0,0 +1,116 @@
+/* 
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "OnePlus 12R",
+    img: "img/product-1.png",
+    price: 39999,
+    company: "OnePlus",
+    info:
+      "The Oneplus 12R is an Android-based smartphone manufactured by Chinese company Oneplus. It was announced on January 23, 2024 and released on February 6, 2024. It is the successor of the Oneplus 11R and the first Oneplus device to feature the Qualcomm Snapdragon 8 Gen 2 chipset, the LTPO4 AMOLED display, and the 100W wired charging.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Samsung S24 Ultra",
+    img: "img/product-2.png",
+    price: 124999,
+    company: "SAMSUNG",
+    info:
+      "The Samsung Galaxy S24 Ultra 5G comes with a 6.8-inch touchscreen with 501PPI. It packs a 200-megapixel rear camera and a 12-megapixel selfie camera. It is all powered by the Qualcomm SM8650-AB Snapdragon 8 Gen 3 (4 nm) chipset and 12GB of RAM.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Lava Agni",
+    img: "img/product-3.png",
+    price: 8999,
+    company: "Lava",
+    info:
+      "The Lava Agni 2 features a 6.78-inch curved AMOLED display with a 120Hz refresh rate. It is powered by the MediaTek Dimensity 7050 chipset, has 8GB RAM, and 256GB storage. The quad-camera setup includes a 50MP main sensor, and it runs on Android 13 with promises of future updates. The device also boasts a 4700mAh battery with 66W fast charging.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Nothing Phone2(A)",
+    img: "img/product-4.png",
+    price: 24999,
+    company: "Nothing",
+    info:
+      "The Nothing Phone 2a, stylised as Phone, is an Android smartphone by Nothing. The phone was announced on 5 March, 2024 and released on 12 March, 2024. The Nothing Phone surpassed 100,000 units sold in its first day after release.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Xiaomi 14 CIVI",
+    img: "img/product-5.png",
+    price: 39999,
+    company: "Xiaomi",
+    info:
+      "The Xiaomi 14 Civi features a 50MP Leica-co-engineered main camera, a 50MP telephoto lens, and a 12MP ultra-wide camera. It has dual 32MP front cameras, one with an ultra-wide lens. The phone is powered by the Snapdragon 8s Gen 3 processor, and it sports a 6.55-inch AMOLED display with Dolby Vision and HDR10+. It runs on Android 14-based HyperOS with promises of three years of OS updates and four years of security updates.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Motorola Edge 50 Plus",
+    img: "img/product-6.png",
+    price: 21999,
+    company: "Motorola",
+    info:
+      "The Motorola Edge 50 Plus features a 6.7-inch pOLED display with a 1.5K resolution and 144Hz refresh rate, providing vibrant and smooth visuals. It is powered by the Snapdragon 8s Gen 3 chipset and comes with a 4500mAh battery. The camera setup includes a 50MP main sensor, 13MP ultra-wide lens, and advanced AI features like adaptive stabilization and auto-focus tracking. Additionally, the device offers up to 12GB of RAM and a Pantone validated display for accurate color reproduction.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Iphone 14 Plus",
+    img: "img/product-7.png",
+    price: 54999,
+    company: "apple",
+    info:
+      "The iPhone 14 Plus features a 6.7-inch Super Retina XDR display and is powered by the A15 Bionic chip with a 5-core GPU, offering robust performance and enhanced graphics capabilities. It comes with a dual-camera system, including a 12MP main sensor and an ultra-wide lens, providing improved low-light performance and high-quality photo and video capabilities. The device supports 5G connectivity, has a battery life of up to 26 hours of video playback, and includes features like MagSafe wireless charging and emergency SOS via satellite. The iPhone 14 Plus runs on iOS 17, ensuring up-to-date software and security features.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "iPhone 15 Pro Max",
+    img: "img/product-8.png",
+    price: 154999,
+    company: "Apple",
+    info:
+      "New Apple iPhone 15 Pro Max. Aerospace-grade titanium design, making it both strong and light. USB-C connector. All-day battery life up to 29hrs video playback. 48MP main camera with multiple focal lengths, like having seven pro lenses in your pocket. Telephoto lens with 5x optical zoom. Customizable Action button for your favorite feature. A17 Pro chip, pro-class GPU with 6 cores.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Google Pixel 8A",
+  img: "img/product-1.png",
+  price: 35999,
+  company: "google",
+  info:
+    "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
